bf53089c5d4fe110b519ea47bf79d329c0427f0a
enzoo96 <enzoo.site@gmail.com>
2018-03-21 15:18:03 +0000

    Revert "usb: gadget: f_fs: Add ioctl for allocating endpoint buffers."
    This reverts commit 16892d7c10f4ea8d959bc5fce66f496e5f9bfdcb.
    
    Change-Id: Ibb5120d1d8d306a5bf61ee2158e2d959a91640bf

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -137,9 +137,6 @@ struct ffs_epfile {
 
 	unsigned char			_pad;
 	atomic_t			opened;
-
-	unsigned long			buf_len;
-	char				*buffer;
 };
 
 /*  ffs_io_data structure ***************************************************/
@@ -802,11 +799,10 @@ retry:
 		spin_unlock_irq(&epfile->ffs->eps_lock);
 
 		if (!io_data->read)
-			data_len += extra_buf_alloc;
-
-		data = (data_len > epfile->buf_len || io_data->aio) ?
-			kmalloc(data_len, GFP_KERNEL) :
-			epfile->buffer;
+			data = kmalloc(data_len + extra_buf_alloc,
+					GFP_KERNEL);
+		else
+			data = kmalloc(data_len, GFP_KERNEL);
 		if (unlikely(!data))
 			return -ENOMEM;
 		if (io_data->aio && !io_data->read) {
@@ -996,8 +992,7 @@ retry:
 					}
 				}
 			}
-			if (data_len > epfile->buf_len || io_data->aio)
-				kfree(data);
+			kfree(data);
 		}
 	}
 
@@ -1008,8 +1003,7 @@ error_lock:
 	spin_unlock_irq(&epfile->ffs->eps_lock);
 	mutex_unlock(&epfile->mutex);
 error:
-	if (data_len > epfile->buf_len || io_data->aio)
-		kfree(data);
+	kfree(data);
 	if (ret < 0)
 		pr_err_ratelimited("Error: returning %zd value\n", ret);
 	return ret;
@@ -1180,7 +1174,7 @@ static long ffs_epfile_ioctl(struct file *file, unsigned code,
 		return -ENODEV;
 
 	spin_lock_irq(&epfile->ffs->eps_lock);
-	if (epfile->ep) {
+	if (likely(epfile->ep)) {
 		switch (code) {
 		case FUNCTIONFS_FIFO_STATUS:
 			ret = usb_ep_fifo_status(epfile->ep->ep);
@@ -1218,29 +1212,11 @@ static long ffs_epfile_ioctl(struct file *file, unsigned code,
 				ret = -EFAULT;
 			return ret;
 		}
-		case FUNCTIONFS_ENDPOINT_ALLOC:
-			kfree(epfile->buffer);
-			epfile->buffer = NULL;
-			epfile->buf_len = value;
-			if (epfile->buf_len) {
-				epfile->buffer = kzalloc(epfile->buf_len,
-						GFP_KERNEL);
-				if (!epfile->buffer)
-					ret = -ENOMEM;
-			}
-			break;
-
 		default:
 			ret = -ENOTTY;
 		}
 	} else {
-		switch (code) {
-		case FUNCTIONFS_ENDPOINT_ALLOC:
-			epfile->buf_len = value;
-			break;
-		default:
-			ret = -ENODEV;
-		}
+		ret = -ENODEV;
 	}
 	spin_unlock_irq(&epfile->ffs->eps_lock);
 
@@ -1808,8 +1784,6 @@ static void ffs_func_eps_disable(struct ffs_function *func)
 			ep->ep->driver_data = NULL;
 		}
 		epfile->ep = NULL;
-		kfree(epfile->buffer);
-		epfile->buffer = NULL;
 
 		++ep;
 		++epfile;
@@ -1850,14 +1824,6 @@ static int ffs_func_eps_enable(struct ffs_function *func)
 
 		ep->ep->driver_data = ep;
 		ep->ep->desc = ds;
-		if (epfile->buf_len) {
-			epfile->buffer = kzalloc(epfile->buf_len,
-					GFP_KERNEL);
-			if (!epfile->buffer) {
-				ret = -ENOMEM;
-				break;
-			}
-		}
 
 		ret = config_ep_by_speed(func->gadget, &func->function, ep->ep);
 		if (ret) {
